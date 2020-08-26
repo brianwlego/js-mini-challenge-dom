22a7e7ca8d55042c54798f92f1daabf94c1fad4f
brianwlego <legobw@gmail.com>
2020-08-25 21:22:32 -0400

    Finished Deliverables

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+header.style.color = "red"
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+const outerDiv = document.getElementsByClassName('player-container')[0]
+PLAYERS.forEach(function(player){
+  const newDiv = document.createElement('div')
+  newDiv.classList = "player"
+  newDiv.dataset.number = `${player.number}`
+  newDiv.innerHTML = `
+    <h3>${player.name} (<em>${player.nickname}</em>)
+    </h3>
+    <img src="${player.photo}" alt="${player.name}">
+    `
+  outerDiv.insertAdjacentElement('afterbegin', newDiv)
+})
+
 
 
 /***** Deliverable 4 *****/
+
+document.querySelector("div.player[data-number='7']").remove()
+
